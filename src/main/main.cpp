e56c5a97845ea98f5ef449e06abef49098c44aac
CarlosMi11 <carlosmirandaucv@gmail.com>
2024-11-07 21:26:57 -0400

    diagrama inicial

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
     int main = 2;
     cout<<main<<endl;
+    vector<int> a;
+    cout<<a.max_size()<<endl;
+    vector<char> b;
+    cout<<b.max_size()<<endl;
 }
